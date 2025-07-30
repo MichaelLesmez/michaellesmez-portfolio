2f346357405019b63699b8fd26ce9d2d96306d91
MichaelLesmez <Michaellesmez@gmail.com>
2025-07-29 21:20:14 -0400

    your name

--- frontend-developer-light-portfolio-template/app/layout.tsx
+++ frontend-developer-light-portfolio-template/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-	title: '[Your Name] - Frontend Developer',
+	title: 'Michael Lesmez - Full StackDeveloper',
 	description:
 		'Welcome to my portfolio! I am a passionate frontend developer specializing in creating beautiful, responsive, and user-centric web applications. With expertise in modern JavaScript frameworks and UI/UX principles, I transform designs into seamless interactive experiences.',
 	keywords: [
@@ -31,21 +31,21 @@ export const metadata: Metadata = {
 		'Performance Optimization',
 		'Web Accessibility',
 		'Component Design',
-		'[Your Name]',
+		'Michael Lesmez',
 	],
-	authors: [{ name: '[Your Name]' }],
-	creator: '[Your Name]',
+	authors: [{ name: 'Michael Lesmez' }],
+	creator: 'Michael Lesmez',
 	openGraph: {
-		title: '[Your Name] - Frontend Developer Portfolio',
+		title: 'Michael Lesmez - Frontend Developer Portfolio',
 		description: 'Passionate frontend developer crafting beautiful and interactive web experiences. Explore my projects and frontend development expertise.',
 		url: 'https://your-domain.com',
-		siteName: '[Your Name] - Portfolio',
+		siteName: 'Michael Lesmez - Portfolio',
 		images: [
 			{
 				url: '/og-image.jpg',
 				width: 1200,
 				height: 630,
-				alt: '[Your Name] - Frontend Developer Portfolio',
+				alt: 'Michael Lesmez - Frontend Developer Portfolio',
 			},
 		],
 		locale: 'en_US',
@@ -53,7 +53,7 @@ export const metadata: Metadata = {
 	},
 	twitter: {
 		card: 'summary_large_image',
-		title: '[Your Name] - Frontend Developer',
+		title: 'Michael Lesmez - Frontend Developer',
 		description: 'Passionate frontend developer crafting beautiful and interactive web experiences. Explore my projects and frontend development expertise.',
 		creator: '@yourusername',
 		images: ['/og-image.jpg'],
